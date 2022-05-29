cbb73856e2396535ad237ad8a028149da2945d43
losman333 <losman333@hotmail.com>
2022-05-28 22:24:05 -0700

    dbstuff

--- core/settings.py
+++ core/settings.py
@@ -108,25 +108,25 @@ WSGI_APPLICATION = 'core.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
-
 # DATABASES = {
 #     'default': {
-#         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-#         'NAME': config('DB_NAME'),
-#         'HOST': config('DB_HOST'),
-#         'PORT': config('DB_PORT'),
-#         'USER': config('DB_USER'),
-#         'URL': config('DB_URI'),
-#         'PASSWORD': config('DB_PASSWORD')
-
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': BASE_DIR / 'db.sqlite3',
 #     }
-# }  
+# }
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': config('DB_NAME'),
+        'HOST': config('DB_HOST'),
+        'PORT': config('DB_PORT'),
+        'USER': config('DB_USER'),
+        'URL': config('DB_URI'),
+        'PASSWORD': config('DB_PASSWORD')
+
+    }
+}  
 
 
 # Password validation
